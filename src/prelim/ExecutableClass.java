29e670ac43704ca2a4b1ba93b1c93f41ca68e484
Shanthal <2227297@slu.edu.ph>
2023-09-05 10:22:36 +0800

    Worked on MyFixedSizeArrayList

--- src/prelim/ExecutableClass.java
+++ src/prelim/ExecutableClass.java
@@ -1,4 +1,9 @@
 package prelim;
 
 public class ExecutableClass {
+    public static void main(String[] args) {
+        MyFixedSizeArrayList object = new MyFixedSizeArrayList();
+
+        System.out.println(object.getSize());
+    }
 }
--- src/prelim/MyFixedSizeArrayList.java
+++ src/prelim/MyFixedSizeArrayList.java
@@ -1,5 +1,36 @@
 package prelim;
 
-public class MyFixedSizeArrayList{
+import java.util.NoSuchElementException;
 
+public class MyFixedSizeArrayList implements MyList<Object>{
+
+    private Object[] arrayObject = new String[5];
+    private int indexCount = 0;
+
+    public int getSize() {
+        return arrayObject.length;
+    }
+
+    public void insert(Object data) {
+        arrayObject[indexCount] = data;
+        indexCount ++;
+    }
+
+    //public Object getElements(Object data) throws NoSuchElementException {}
+
+    /** UNFINISHED **/
+    public boolean delete(Object deleteKey) {
+        for (Object obj: arrayObject) {
+            if (obj == deleteKey)
+                return true;
+        }
+        return false;
+    }
+
+    public int search(Object search) {
+        for (int x =0; x <arrayObject.length; x++)
+            if (arrayObject[x] == search)
+                return x;
+        return -1;
+    }
 }
