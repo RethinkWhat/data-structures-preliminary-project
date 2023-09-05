596e0970e16fc3a0795a0d85d2c00e43f05e3fca
Shanthal <2227297@slu.edu.ph>
2023-09-05 10:01:43 +0800

    Reset files

--- src/prelim/MyFixedSizeArrayList.java
+++ src/prelim/MyFixedSizeArrayList.java
@@ -1,25 +1,5 @@
 package prelim;
 
-public class MyFixedSizeArrayList {
-
-    public class personalProperties implements MyList<personalProperties> {
-        private String itemName;
-        private String modelNumber;
-        private String color;
-        private boolean status;
-
-        public int getSize() {}
-
-        public void insert(personalProperties itemToInsert) {}
-
-        public personalProperties getElement(personalProperties itemToBeFound){}
-
-        public boolean delete(personalProperties itemToBeDeleted){}
-
-        public int search(personalProperties itemToBeFound){}
-
-
-    }
-
+public class MyFixedSizeArrayList{
 
 }
